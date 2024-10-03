4da8d0716aff3c6dc6e75038bbc71a57e1139c2c
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-10-03 15:27:29 -0300

    criando rotas de get e put para motoristas

--- src/autocare.api/routes/Motoristas.js
+++ src/autocare.api/routes/Motoristas.js
@@ -1,12 +1,12 @@
 import express from 'express'
+
 import { PrismaClient } from '@prisma/client'
-import { log } from 'console'
 
 const prisma = new PrismaClient()
 const router = express.Router()
 
 // ROTA DE CADASTRO DE MOTORISTA
-router.post('/cadastro-motoristas', async (req, res) => {
+router.post('/motoristas', async (req, res) => {
   try {
     const motorista = req.body;
 
@@ -29,11 +29,55 @@ router.post('/cadastro-motoristas', async (req, res) => {
     res.status(201).json(motoristaDB);
 
   } catch (err) {
-    console.log(err);
+    console.log(err)
     res.status(500).json({ message: 'Erro ao cadastrar motorista' });
   }
-
 })
+
+// ROTA DE BUSCA DE MOTORISTAS
+router.get("/motoristas", async (req, res) => {
+  try {
+    const motoristas = await prisma.motoristas.findMany();
+    res.status(200).json(motoristas)
+  } catch (err) {
+    console.error("Erro ao buscar motoristas:", err)
+    res.status(500).json({ message: 'Erro ao buscar motoristas', error: err.message });
+  }
+});
+
+// ROTA DE ALTERAÇÃO DE MOTORISTA
+router.put('/motoristas/:id', async (req, res) => {
+  try {
+    const motorista = req.body;
+    const id = req.params.id;
+    console.log(motorista);
+
+    if (motorista.status) {
+      const statusValido = ["ATIVO", "INATIVO"].includes(motorista.status.toUpperCase());
+      if (!statusValido) {
+        return res.status(400).json({ message: 'Status inválido. Use "ATIVO" ou "INATIVO".' });
+      }
+    }
+
+    const motoristaDB = await prisma.motoristas.update({
+      where: {
+        id: id
+      },
+      data: {
+        nome: motorista.nome,
+        cnh: motorista.cnh,
+        telefone: motorista.telefone,
+        dataNascimento: motorista.dataNascimento,
+        status: motorista.status ? motorista.status.toUpperCase() : "ATIVO"
+      }
+    });
+    res.status(200).json(motoristaDB);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Erro ao alterar motorista' });
+  }
+});
 
 
 export default router;
