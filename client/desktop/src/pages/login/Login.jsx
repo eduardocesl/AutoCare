b5e63359574b80d71155998104714dafd1015b7b
eduardocesl <eduardocesl@outlook.com>
2024-10-18 02:07:23 -0300

    Ajustes no layout da tela de login

--- client/desktop/src/pages/login/Login.jsx
+++ client/desktop/src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import './Login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -28,34 +29,39 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="login-container">
+            <div className="login-block">
+            <h2 className="login-title">Login</h2>
 
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label>Email:</label>
+            <form className="login-form" onSubmit={handleLogin}>
+                <div className="input-group">
+                    <label>E-mail</label>
                     <input
+                        className="login-input"
                         type="email"
+                        placeholder='E-mail'
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         required
                     />
                 </div>
-                <div>
-                    <label>Senha:</label>
+                <div className="input-group">
+                    <label>Senha</label>
                     <input
                         type="password"
+                        placeholder='Senha'
                         value={senha}
                         onChange={(e) => setSenha(e.target.value)}
                         required
                     />
                 </div>
-                <button type="submit">Entrar</button>
+                <button className="login-button" type="submit">LOGIN</button>
             </form>
 
             {/* Adicionei para mostrar mensagens de erro e sucesso */}
             {loginSucesso && <p style={{ color: 'green' }}>Login realizado com sucesso!</p>}
             {erroLogin && <p style={{ color: 'red' }}>{erroLogin}</p>}
+        </div>
         </div>
     );
 };
