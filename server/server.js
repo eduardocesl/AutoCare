60d1f8f20b06faa261a3d7aa41955aa41d433312
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-10-18 10:46:45 -0300

    unificando tudo

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ import express from 'express'
 import Usuarios from './src/autocare.api/routes/Usuarios.js'
 import Motoristas from './src/autocare.api/routes/Motoristas.js'
 import Veiculos from './src/autocare.api/routes/Veiculos.js'
+import cors from 'cors'
 
 import auth from './src/autocare.api/middlewares/auth.js'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
+
 
 app.use('/', Usuarios)
 app.use('/', Motoristas)
--- server/src/autocare.api/routes/Veiculos.js
+++ server/src/autocare.api/routes/Veiculos.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 
+
 const prisma = new PrismaClient();
 const router = express.Router();
 
