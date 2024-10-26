7cab1cf635346678dae4b3d27a29c4cff64306b7
lucas-alamis <lucas.alamis@gmail.com>
2024-10-26 15:00:59 -0300

    Altereçao Menu

--- client/desktop/src/components/Navbar.jsx
+++ client/desktop/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="app-navbar">
       <ul>
-        <li><a href="/">Dashboard</a></li>
+        <li><a href="/">Motoristas</a></li>
         <li><a href="/veiculos">Veículos</a></li>
         <li><a href="/perfil">Perfil do Usuário</a></li>
         <li><a href="/settings">Configurações</a></li>
